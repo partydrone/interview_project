8ff4b9bd954cf92ec5927224906a386689cc90a7
Kyle Shipley <kshipley@hellobrightline.com>
2021-07-14 20:37:49 -0400

    Add controller / routes / specs for ExerciseAssignment

---
+++ app/controllers/exercise_assignments_controller.rb
@@ -0,0 +1,36 @@
+class ExerciseAssignmentsController < ApplicationController
+  before_action :set_exercise_assignment, only: [:show, :destroy]
+
+  def index
+    @exercise_assignments = ExerciseAssignment.all
+
+    render json: @exercise_assignments
+  end
+
+  def show
+    render json: @exercise_assignment
+  end
+
+  def create
+    @exercise_assignment = ExerciseAssignment.new(exercise_assignment_params)
+
+    if @exercise_assignment.save
+      render json: @exercise_assignment, status: :created, location: @exercise_assignment
+    else
+      render json: @exercise_assignment.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @exercise_assignment.destroy
+  end
+
+  private
+    def set_exercise_assignment
+      @exercise_assignment = ExerciseAssignment.find(params[:id])
+    end
+
+    def exercise_assignment_params
+      params.require(:exercise_assignment).permit(:exercise_id, :member_id)
+    end
+end
