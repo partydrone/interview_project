2a6b0ae4e8a13927ac91943cd6cbf9a5eea856a2
Kyle Shipley <kshipley@hellobrightline.com>
2022-01-10 17:23:40 -0500

    Make exercise names more realistic

--- spec/models/exercise_spec.rb
+++ spec/models/exercise_spec.rb
@@ -3,13 +3,13 @@ require "rails_helper"
 RSpec.describe Exercise do
   describe ".find" do
     it "returns the exercise with the given id" do
-      exercise = described_class.find(:self_calming_through_relaxation_handout)
-      expect(exercise.id).to eq(:self_calming_through_relaxation_handout)
+      exercise = described_class.find(:all_about_me)
+      expect(exercise.id).to eq(:all_about_me)
     end
 
     it "works with strings and symbols" do
-      expect(described_class.find(:self_calming_through_relaxation_handout))
-        .to eq(described_class.find("self_calming_through_relaxation_handout"))
+      expect(described_class.find(:all_about_me))
+        .to eq(described_class.find("all_about_me"))
     end
   end
 end
